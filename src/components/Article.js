4b96bfebc6c1d5378d8416ff03e15faacec31f82
phuocle2 <issah.le@gmail.com>
2025-07-31 16:20:35 -0500

    Added intendation

--- src/components/Article.js
+++ src/components/Article.js
@@ -14,19 +14,61 @@ export const Article = () => {
 
     const url = useCtx.svtk_bot;
     const { data, error, loading } = useAxios({url, 'data': useCtx.vpns});
-        
+
+    console.log('data:', data);
+    
     return (
         <React.Fragment>
             <div>Sống Với Thánh Kinh</div>
             <div>
                 {loading && <p>Loading...</p>}
                 {!loading && error && <p>{error}</p>}
-                {!loading && !error && data && <p>{data?.title}</p>}
-                {!loading && !error && data && <p>{data?.message}</p>}
                 {!loading && !error && !data && <p>No data.</p>}
+                {/* {!loading && !error && data && <p>{data?.message}</p>} */}
                 {/* {data && <p>{data?.title}</p>}
                 {data && <p>{data?.message}</p>}
                 {!data && <p>No data.</p>} */}
+
+                {!loading && !error && data && <p>{data?.title}</p>}
+
+                {/* {data && data?.message.length} */}
+
+                <p>
+                    {!loading && !error && data && data?.message.slice(0, 1).map((d, index) => {
+                        return (
+                            <span key={index}>{d}</span>
+                        );
+                    })}
+                </p>
+
+                <p>
+                    {!loading && !error && data && data?.message.slice(1, 3).map((d, index) => {
+                        return (
+                            <span key={index}>{d}</span>
+                        );
+                    })}
+                </p>
+
+                <p>
+                    {!loading && !error && data && data?.message.slice(3, 5).map((d, index) => {
+                        return (
+                            <span key={index}>{d}</span>
+                        );
+                    })}
+                </p>
+
+                {!loading && !error && data && data?.message.slice(5, data && data?.message.length - 1).map((d, index) => {
+                    return (
+                        <p key={index}>{d}</p>
+                    );
+                })}
+
+                {!loading && !error && data && data?.message.slice(data && data?.message.length - 1).map((d, index) => {
+                    return (
+                        <p key={index}>{d}</p>
+                    );
+                })}
+
             </div>
         </React.Fragment>
     );
