7836db31f2794650a2962eae5780c5acd0686a30
phuocle2 <issah.le@gmail.com>
2025-07-31 17:46:27 -0500

    Added new styles

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,7 +6,7 @@ import useAxios from './hooks/useAxios';
 import AuthContext from './store/auth';
 
 // CSS style
-
+import './Article.css';
 
 export const Article = () => {
     // console.log("Article rendering");
@@ -19,56 +19,56 @@ export const Article = () => {
     
     return (
         <React.Fragment>
-            <div>Sống Với Thánh Kinh</div>
             <div>
-                {loading && <p>Loading...</p>}
-                {!loading && error && <p>{error}</p>}
-                {!loading && !error && !data && <p>No data.</p>}
-                {/* {!loading && !error && data && <p>{data?.message}</p>} */}
-                {/* {data && <p>{data?.title}</p>}
-                {data && <p>{data?.message}</p>}
-                {!data && <p>No data.</p>} */}
+                <div><h1>Sống Với Thánh Kinh</h1></div>
+                <div id='content_card'>
+                    {loading && <p>Loading...</p>}
+                    {!loading && error && <p>{error}</p>}
+                    {!loading && !error && !data && <p>No data.</p>}
+                    {/* {!loading && !error && data && <p>{data?.message}</p>} */}
+                    {/* {data && <p>{data?.title}</p>}
+                    {data && <p>{data?.message}</p>}
+                    {!data && <p>No data.</p>} */}
 
-                {!loading && !error && data && <p>{data?.title}</p>}
+                    {!loading && !error && data && <p><h2>{data?.title}</h2></p>}
 
-                {/* {data && data?.message.length} */}
+                    {/* {data && data?.message.length} */}
 
-                <p>
-                    {!loading && !error && data && data?.message.slice(0, 1).map((d, index) => {
+                    <p>
+                        {!loading && !error && data && data?.message.slice(0, 1).map((d, index) => {
+                            return (
+                                <span key={index}>{d}</span>
+                            );
+                        })}
+                    </p>
+
+                    <p>
+                        {!loading && !error && data && data?.message.slice(1, 3).map((d, index) => {
+                            return (
+                                <span key={index}>{d}</span>
+                            );
+                        })}
+                    </p>
+
+                    <p>
+                        {!loading && !error && data && data?.message.slice(3, 5).map((d, index) => {
+                            return (
+                                <span key={index}>{d}</span>
+                            );
+                        })}
+                    </p>
+
+                    {!loading && !error && data && data?.message.slice(5, data && data?.message.length - 1).map((d, index) => {
                         return (
-                            <span key={index}>{d}</span>
+                            <p key={index}>{d}</p>
                         );
                     })}
-                </p>
-
-                <p>
-                    {!loading && !error && data && data?.message.slice(1, 3).map((d, index) => {
-                        return (
-                            <span key={index}>{d}</span>
-                        );
-                    })}
-                </p>
-
-                <p>
-                    {!loading && !error && data && data?.message.slice(3, 5).map((d, index) => {
-                        return (
-                            <span key={index}>{d}</span>
-                        );
-                    })}
-                </p>
-
-                {!loading && !error && data && data?.message.slice(5, data && data?.message.length - 1).map((d, index) => {
-                    return (
-                        <p key={index}>{d}</p>
-                    );
-                })}
-
+                </div>
                 {!loading && !error && data && data?.message.slice(data && data?.message.length - 1).map((d, index) => {
                     return (
                         <p key={index}>{d}</p>
                     );
                 })}
-
             </div>
         </React.Fragment>
     );
